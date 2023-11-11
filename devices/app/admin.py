955c417e1577841094c2b122b973463fd778026a
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2023-11-11 12:42:42 +0300

    фÐall location model, migrate data to it, add relation between device and location

--- devices/app/admin.py
+++ devices/app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Device, Comment
+from .models import Device, Comment, Location
 
 
 class CommentInline(admin.TabularInline):
@@ -10,12 +10,19 @@ class CommentInline(admin.TabularInline):
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sticker', 'building', 'ip', 'block', 'model', 'netmask', 'sn', 'mac', 'street')
-    search_fields = ('id', 'sticker', 'building', 'ip', 'block', 'model', 'netmask', 'sn', 'mac', 'street')
+    list_display = ('id', 'sticker', 'ip', 'model', 'netmask', 'sn', 'mac', 'state', 'location')
+    search_fields = ('id', 'sticker', 'ip', 'model', 'netmask', 'sn', 'mac', 'state', 'location')
     inlines = (CommentInline,)
+    list_per_page = 10
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'text', 'device', 'published')
     search_fields = ('id', 'text', 'device', 'published')
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'block', 'street', 'building')
+    search_fields = ('id', 'block', 'street', 'building')
